759a707d8edc199159096045b8a3b267786c9b30
Cwait00 <cwaitanobongoza@gmail.com>
2024-02-04 14:23:39 -0800

    updated

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -4,7 +4,8 @@ import os
 from fabric.api import *
 
 env.hosts = ["104.196.168.90", "35.196.46.172"]
-
+env.user = "ubuntu"
+env.key_filename = "~/.ssh/id_rsa"
 
 def do_clean(number=0):
     """Delete out-of-date archives.
@@ -18,13 +19,13 @@ def do_clean(number=0):
     """
     number = 1 if int(number) == 0 else int(number)
 
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
+    # Local clean-up
     with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+        local("ls -t | tail -n +{} | xargs -I {{}} rm -- {{}}".format(number + 1))
 
+    # Remote clean-up
     with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        run("ls -tr | tail -n +{} | xargs -I {{}} rm -rf -- {{}}".format(number + 1))
+
+if __name__ == "__main__":
+    do_clean()
