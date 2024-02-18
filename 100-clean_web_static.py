e8533994bb12d0743faa4eeea1a8532163f2e360
Cwait00 <cwaitanobongoza@gmail.com>
2024-02-18 07:57:59 -0800

    updated

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -17,6 +17,7 @@ def do_clean(number=0):
     number is 2, keeps the most and second-most recent archives,
     etc.
     """
+
     number = 1 if int(number) == 0 else int(number)
 
     # Local clean-up
