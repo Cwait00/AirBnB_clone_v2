cbd72bc7367eb876bc27f201b0b4f9cb4971667e
Cwait00 <cwaitanobongoza@gmail.com>
2024-02-18 05:45:51 -0800

    updated

--- models/state.py
+++ models/state.py
@@ -1,35 +1,23 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
-import models
-from models.city import City
-import shlex
 
+from typing import List
+from models.base_model import BaseModel, Base
+from models.city import City
+import models
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
     """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
-
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+    State class for AirBnB project.
+    """
+    if models.storage_type != 'db':
+        def cities(self) -> List[City]:
+            """
+            Getter method to return the list of City objects
+            from storage linked to the current State.
+            """
+            from models import storage
+            city_objs = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_objs.append(city)
+            return city_objs
